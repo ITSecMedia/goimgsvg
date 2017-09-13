e6c712b3705250cdeed76c899aefbb3529151740
ITSecMedia <info@itsecmedia.com>
2017-09-13 12:34:18 +0200

    Initialize project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # goimgsvg
-SVG as base64 inline images
+
+SVG as base64 inline images for use in Go-Lang templates
+
+To be compatible with Microsoft Edge the SVG still have to be base64 encoded as currently only Chrome supports pure SVG as inline image.
+
+<https://css-tricks.com/probably-dont-base64-svg/>
+
+<https://css-tricks.com/lodge/svg/09-svg-data-uris/>
+
+Example:
+
+// Create instance - better call only once because of the performance bottleneck
+svg := goimgsvg.NewGoImgSVG()
+
+// Print an svg based on svg filename in the folder ./assets/goimgsvg/svg/
+flag := svg.GetSVGByFilename("eur")
+
+// Print all .tld-svg related supplied currency tru translation in the folder ./assets/goimgsvg/tld_cur.tsv
+flag := svg.GetSVGByCurrency("eur")
+
+![Example 1](./2017-09-13_12-31-32.png)
+
+![Example 1](./2017-09-13_12-32-00.png)
---
+++ goimgsvg.go
@@ -0,0 +1,133 @@
+package goimgsvg
+
+import (
+	"encoding/base64"
+	"encoding/csv"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+// LoadMapCurrencyTld ...
+func loadMapCurrencyTld() (map[string][]string, error) {
+
+	f, err := os.Open("./assets/goimgsvg/tld_cur.tsv")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	currencyTld := make(map[string][]string)
+
+	// https://golang.org/pkg/encoding/csv/
+	csvr := csv.NewReader(f)
+	csvr.Comma = '\t'
+
+	lines, err := csvr.ReadAll()
+	if err != nil {
+		return currencyTld, err
+	}
+
+	for _, line := range lines {
+		tld := strings.TrimPrefix(line[0], ".")
+		cur := strings.Split(strings.ToLower(line[1]), ",")
+		for _, val := range cur {
+			currencyTld[val] = append(currencyTld[val], tld)
+		}
+
+	}
+
+	return currencyTld, nil
+
+}
+
+func loadMapFilenameSVGBase64() (map[string]string, error) {
+
+	svg := make(map[string]string)
+	svgPath := "./assets/goimgsvg/svg"
+
+	files, err := ioutil.ReadDir(svgPath)
+	if err != nil {
+		// log.Fatal(err)
+		return svg, err
+	}
+
+	for _, file := range files {
+		svgFileName := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+		svgContent, err := ioutil.ReadFile(svgPath + "/" + file.Name())
+		if err != nil {
+			// log.Fatal(err)
+			return svg, err
+		}
+		svg[svgFileName] = string(base64.StdEncoding.EncodeToString(svgContent))
+	}
+
+	return svg, nil
+
+}
+
+// GoImgSVG ...
+type GoImgSVG struct {
+	Base64SVG   map[string]string // [iso3166]base64
+	CurrencyTLD map[string][]string
+}
+
+// currencyToISO3166 ...
+func (cf *GoImgSVG) currencyToISO3166(currency string) []string {
+
+	return cf.CurrencyTLD[currency]
+
+}
+
+// printSVG ...
+func (cf *GoImgSVG) printSVG(base64 string, alt string) string {
+
+	img := "<div class='goimgsvg'><img src='data:image/svg+xml;base64," + base64 + "' alt='" + alt + "'/></div>"
+	return img
+
+}
+
+// GetSVGByCurrency ...
+func (cf *GoImgSVG) GetSVGByCurrency(currency string) string {
+
+	tld := cf.currencyToISO3166(currency)
+	var img string
+	for _, v := range tld {
+		img += cf.GetSVGByFilename(v)
+	}
+	return img
+
+}
+
+// GetSVGByFilename ...
+func (cf *GoImgSVG) GetSVGByFilename(iso3166 string) string {
+
+	svg := cf.Base64SVG[iso3166]
+	return cf.printSVG(svg, iso3166)
+
+}
+
+// NewGoImgSVG returns a CountrySVGs instance with a pre-cached flags-hashmap
+// Only call once at the beginning because it's slow and therefore expensive
+func NewGoImgSVG() *GoImgSVG {
+
+	base64SVG, err := loadMapFilenameSVGBase64()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currencyTld, err := loadMapCurrencyTld()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cf := &GoImgSVG{
+		Base64SVG:   base64SVG,
+		CurrencyTLD: currencyTld,
+	}
+
+	return cf
+
+}
